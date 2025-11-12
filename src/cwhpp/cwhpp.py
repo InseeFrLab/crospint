294f0040056ae458ed455f5100851cb8a1181a33
k2kfed <olivier.meslin@insee.fr>
2025-11-12 16:14:53 +0000

    Add dependency

--- src/cwhpp/cwhpp.py
+++ src/cwhpp/cwhpp.py
@@ -25,6 +25,7 @@ import time
 import math
 import numpy as np
 import polars as pl
+from polars import col as c
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
