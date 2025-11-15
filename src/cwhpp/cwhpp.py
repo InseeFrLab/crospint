4929487786ca67a53ea266de366cec321a999f32
k2kfed <olivier.meslin@insee.fr>
2025-11-15 00:32:44 +0000

    New version

--- src/cwhpp/cwhpp.py
+++ src/cwhpp/cwhpp.py
@@ -750,37 +750,40 @@ but the name of the floor area variable is missing")
         self,
         y=None,
         y_pred=None,
+        floor_area=None,
         quantile_start: float = 0,
         quantile_end: float = 1
     ):
 
         self.assert_is_1d_array(y)
         self.assert_is_1d_array(y_pred)
-        if y is None or y_pred is None:
+        self.assert_is_1d_array(floor_area)
+        if y is None or y_pred is None or floor_area is None:
             print(f"Calibrating the model using {self.source_correction_terms} data used \
 in training")
             y = self.y_calibration
             y_pred = self.y_pred_calibration
+            floor_area = self.floor_area_calibration
 
         # Fit the calibration function on the whole distribution
         cal_func = IsotonicRegression(out_of_bounds="clip")
         cal_func.fit(
-            np.sort(y_pred),
-            np.sort(y) / np.sort(y_pred)
+            np.sort(y_pred / floor_area),
+            np.sort(y / floor_area) / np.sort(y_pred / floor_area)
         )
 
         if quantile_start > 0 or quantile_end < 1:
             print(f"Restricting the calibration to the [{quantile_start}; {quantile_end}] range")
-            lower_bound = cal_func.predict(np.quantile(y_pred, [quantile_start])).tolist()[0]
-            upper_bound = cal_func.predict(np.quantile(y_pred, [quantile_end])).tolist()[0]
+            lower_bound = cal_func.predict(np.quantile(y_pred / floor_area, [quantile_start])).tolist()[0]
+            upper_bound = cal_func.predict(np.quantile(y_pred / floor_area, [quantile_end])).tolist()[0]
             cal_func = IsotonicRegression(
                 out_of_bounds="clip",
                 y_min=lower_bound,
                 y_max=upper_bound
             )
             cal_func.fit(
-                np.sort(y_pred),
-                np.sort(y) / np.sort(y_pred)
+                np.sort(y_pred / floor_area),
+                np.sort(y / floor_area) / np.sort(y_pred / floor_area)
             )
 
         self.calibration_function = cal_func
@@ -818,7 +821,8 @@ in training")
             print("    The models includes a calibration step.") \
 
             # Calibrate the data
-            y_pred_calibrated = y_pred * self.calibration_function.predict(y_pred)
+            y_pred_calibrated = y_pred \
+                * self.calibration_function.predict(y_pred / X[self.floor_area_name].to_numpy())
 
             return y_pred_calibrated
 
