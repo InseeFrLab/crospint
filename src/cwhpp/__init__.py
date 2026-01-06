d1b169af56be3a7a18d5af17ff1b434cde1dc186
k2kfed <olivier.meslin@insee.fr>
2026-01-06 15:14:57 +0000

    Adjust init.py

--- src/cwhpp/__init__.py
+++ src/cwhpp/__init__.py
@@ -1,10 +1,10 @@
-## Allow Importing directly from cwhpp
+# Allow Importing directly from cwhpp
 
-from .cwhpp import TwoStepsModel as TwoStepsModel
-from .cwhpp import ValidateFeatures as ValidateFeatures
-from .cwhpp import AddCoordinatesRotation as AddCoordinatesRotation
-from .cwhpp import ConvertDateToInteger as ConvertDateToInteger
-from .cwhpp import ConvertToPandas as ConvertToPandas
-from .cwhpp import create_model_pipeline as create_model_pipeline
-from .cwhpp import create_calibration_pipeline as create_calibration_pipeline
-from .cwhpp import compute_calibration_ratios as compute_calibration_ratios
+from .interpolation import ValidateFeatures as ValidateFeatures
+from .interpolation import AddCoordinatesRotation as AddCoordinatesRotation
+from .interpolation import ConvertDateToInteger as ConvertDateToInteger
+from .interpolation import ConvertToPandas as ConvertToPandas
+from .interpolation import create_model_pipeline as create_model_pipeline
+from .housing_prices import create_calibration_pipeline as create_calibration_pipeline
+from .housing_prices import compute_calibration_ratios as compute_calibration_ratios
+from .housing_prices import TwoStepsModel as TwoStepsModel
